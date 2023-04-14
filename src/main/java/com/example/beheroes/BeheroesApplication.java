acfef8b77429c285ec2f17f05d0ac7bdfc1cc483
emina.marmarac <emina.marmarac@gerniks.com>
2023-04-14 10:34:46 +0200

    initial commit

---
+++ src/main/java/com/example/beheroes/BeheroesApplication.java
@@ -0,0 +1,13 @@
+package com.example.beheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeheroesApplication.class, args);
+	}
+
+}
